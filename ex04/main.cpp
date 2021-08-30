a4f9dd662d2be561b02ae92a7a2885477a94bf72
Desausag <dinar095@hotmail.com>
2021-08-30 19:39:15 +0300

    ex04 has done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,6 +3,7 @@
 
 using std::cout;
 using std::endl;
+using std::getline;
 using std::string;
 
 int main(int ac, char **av)
@@ -15,11 +16,30 @@ int main(int ac, char **av)
 	string	s1 = av[2];
 	string	s2 = av[3];
 	string	filename = av[1];
-//	filename = filename + ".replace";
+	string	line;
 	std::ifstream inf(filename);
 	if (!inf)
+	{
 		cout << "Error file" << endl;
-	std::ofstream outf(filename + "replace");
+		return 1;
+	}
+	std::ofstream outf(filename + ".replace", std::ofstream::trunc);
+	if (!outf)
+	{
+		cout << "Error output file" << endl;
+		return 1;
+	}
+	while (getline(inf, line))
+	{
+		size_t pos = 0;
+		while ((pos = line.find(s1)) != string::npos)
+		{
+			line.erase(pos, s1.size());
+			line.insert(pos, s2);
+		}
+		outf << line << endl;
+	}
 	inf.close();
+	outf.close();
 	return 0;
 }
